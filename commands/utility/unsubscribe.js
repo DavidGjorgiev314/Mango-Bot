69a52c7ff005792d0d72e37444f9c33334af179c
TheHackerMan <davidgorgiev6@gmail.com>
2025-05-26 00:20:33 +0200

    Added prevention for unsubscribing from my server, unless it's me executing the command.

--- commands/utility/unsubscribe.js
+++ commands/utility/unsubscribe.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { canExecuteCommand } = require('../../rate-limiter');
 
 const OWNER_ID = '312920065093664780';
+const OWNER_GUILD_ID = '602663660288213013';
 const channelsFilePath = path.join(__dirname, '../../votd-channels.json');
 
 module.exports = {
@@ -12,8 +13,15 @@ module.exports = {
 		.setName('unsubscribe')
 		.setDescription('Unsubscribe this channel from the daily Bible verse feature.'),
 	async execute(interaction) {
+		if (interaction.guildId === OWNER_GUILD_ID && interaction.user.id !== OWNER_ID) {
+			return await interaction.reply({
+				content: `:no_entry: You cannot unsubscribe from this feature in **David's** server`,
+				ephemeral: true
+			});
+		}
+
 		if (!canExecuteCommand(interaction.user.id, 'unsubscribe', OWNER_ID)) {
-			return await interaction.reply(':hourglass: You can only use this command once every hour.');
+			return await interaction.reply(`:hourglass: You can only use this command once every hour.`);
 		}
 
 		const channelId = interaction.channelId.toString();
@@ -32,12 +40,12 @@ module.exports = {
 			channelData.channelIds = channelData.channelIds.filter(id => id !== channelId);
 			try {
 				fs.writeFileSync(channelsFilePath, JSON.stringify(channelData, null, 2));
-				await interaction.reply(':white_check_mark: Successfully unsubscribed channel from the daily Bible verse feature.');
+				await interaction.reply(`:white_check_mark: Successfully unsubscribed channel from the daily Bible verse feature.`);
 			} catch {
-				await interaction.reply(':x: Failed to unsubscribe. Try again later.');
+				await interaction.reply(`:x: Failed to unsubscribe. Try again later.`);
 			}
 		} else {
-			await interaction.reply(':information_source: This channel is not subscribed to the daily Bible verse feature.');
+			await interaction.reply(`:information_source: This channel is not subscribed to the daily Bible verse feature.`);
 		}
 	},
 };
